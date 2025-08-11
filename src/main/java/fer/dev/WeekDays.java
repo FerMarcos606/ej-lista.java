990895bbb679a4dd2cc04ebe094988ddbd12bc7a
Fer <desarrollowebfer@gmail.com>
2025-08-11 18:37:51 +0000

    init: init repository

---
+++ src/main/java/fer/dev/WeekDays.java
@@ -0,0 +1,16 @@
+package fer.dev;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WeekDays {
+
+    // Campo privado que guarda la lista de días
+    private List<String> days = new ArrayList<>();
+
+    // Método público para obtener la lista de días
+    public List<String> getDays() {
+        return days;
+    }
+
+}
---
+++ src/test/java/fer/dev/WeekDaysTest.java
@@ -0,0 +1,16 @@
+package fer.dev;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class WeekDaysTest {
+
+
+    @Test
+    void testListaInicialNullOVacia() {
+        WeekDays wd = new WeekDays();
+        assertTrue(wd.getDays() == null || wd.getDays().isEmpty());
+    }
+}
+
+
